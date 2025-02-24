79c1b99feaacfa5e264fda05ac49d57295f32b57
MoonTao1 <149588627+MoonTao1@users.noreply.github.com>
2025-02-25 00:28:38 +0800

    Update bulid_models.py

--- utils/bulid_models.py
+++ utils/bulid_models.py
@@ -1,11 +1,11 @@
-from model_cdnn import Model
+from model import Model
 import torch
 from utils.options import parser
 args = parser.parse_args()
 def build_model(args):
     model = None  # 默认返回 None
     if args.seq_len == 1:
-        if args.network == 'salmae':
+        if args.network == 'model':
             model = Model()
     if model is None:
         print("Error: Model is None")
