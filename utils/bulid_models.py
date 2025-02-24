438157fcd7b30f5db7aade43795a29abaccb7176
MoonTao1 <149588627+MoonTao1@users.noreply.github.com>
2025-02-25 00:27:36 +0800

    Add files via upload

---
+++ utils/bulid_models.py
@@ -0,0 +1,12 @@
+from model_cdnn import Model
+import torch
+from utils.options import parser
+args = parser.parse_args()
+def build_model(args):
+    model = None  # 默认返回 None
+    if args.seq_len == 1:
+        if args.network == 'salmae':
+            model = Model()
+    if model is None:
+        print("Error: Model is None")
+    return model
